541a62cf9d8deec6487be5c5bc1f6254c9bdfe11
chitsue1 <160345214+chitsue1@users.noreply.github.com>
2026-01-20 15:47:07 +0000

    Post-Recovery checkpoint

--- replit.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs }: {
-  deps = [
-    pkgs.python311
-    pkgs.python311Packages.pip
-    pkgs.python311Packages.pandas
-    pkgs.python311Packages.ta
-    pkgs.python311Packages.requests
-  ];
-}
